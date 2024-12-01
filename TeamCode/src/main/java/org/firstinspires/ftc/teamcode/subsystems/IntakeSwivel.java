c18bcd497405c486dd4a5db83dd38ed7cccef91a
Faizan Masroor <faizanmasroor01@gmail.com>
2024-11-30 22:43:24 -0600

    Revise IntakeSwivel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSwivel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/IntakeSwivel.java
@@ -11,21 +11,33 @@ public class IntakeSwivel
 
     public Servo servoSwivel;
     public double position;
+    public boolean isInitialized; // Used to ensure position is NonNull to avoid errors in rotCCW() and rotCW()
 
     public IntakeSwivel(HardwareMap hardwareMap)
     {
         servoSwivel = hardwareMap.get(Servo.class, "R2");
         servoSwivel.scaleRange(MIN_POSITION, MAX_POSITION);
+        isInitialized = false;
     }
 
     public void rotCCW()
     {
+        if (!isInitialized)
+        {
+            setPosition(0.5);
+            isInitialized = true;
+        }
         position = Math.min(position + POSITION_INCREMENT, 1);
         servoSwivel.setPosition(position);
     }
 
     public void rotCW()
     {
+        if (!isInitialized)
+        {
+            setPosition(0.5);
+            isInitialized = true;
+        }
         position = Math.max(position - POSITION_INCREMENT, 0);
         servoSwivel.setPosition(position);
     }
